0a2c68649e25d50a9aab08d49158f8882ce5aad1
Abhijit-Rabidas <abhijit.rabidas.mca@gmail.com>
2025-06-06 01:12:14 +0530

    add CORS_ORIGIN

--- server/server.js
+++ server/server.js
@@ -4,7 +4,12 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
